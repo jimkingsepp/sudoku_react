974961e69fcbcc4c543a8015ff5b80be6cde51f9
James Kingsepp <jkingsepp@gmail.com>
2019-01-20 17:44:59 -0500

    initial 9x9 grid

--- src/App.js
+++ src/App.js
@@ -1,28 +1,73 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="game">
+        <div className="game-board">
+          <Board base="9"
+            onClick={i => this.handleClick(i)}
+          />
+        </div>
       </div>
     );
   }
 }
+
+class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: null};
+  }
+
+  render() {
+    return (
+      <button className="square" Id={this.props.id}>
+        {this.state.value}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(this.props.base).fill(null)
+    };
+  }
+
+  renderSquare(i) {
+    return <Square value={this.state.squares[i]} id={i} />;
+  }
+
+  createRow(row) {
+    var squares = [];
+    var len = this.props.base;
+    for(var i = 0; i < len; i++) {
+      squares.push( this.renderSquare(row + i.toString() ));
+    }
+
+    return <div className="board-row">{squares}</div>;
+  }
+
+  render() {
+    return (
+      <div>
+        {this.createRow('A')}
+        {this.createRow('B')}
+        {this.createRow('C')}
+        {this.createRow('D')}
+        {this.createRow('E')}
+        {this.createRow('F')}
+        {this.createRow('G')}
+        {this.createRow('H')}
+        {this.createRow('I')}
+      </div>
+    );
+  }
+}
+
 
 export default App;
