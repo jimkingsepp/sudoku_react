576e2f79d596b0a11882e7256677abed4eb06ce8
James Kingsepp <jkingsepp@gmail.com>
2019-01-30 15:28:45 -0500

    Added code to handle locked values (i.e., one digit) formatting.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,28 @@ const ASCII_A = 65;
 const BASE = 9;
 
 class App extends Component {
+  /*
+  constructor(props) {
+    super(props);
+    this.setUpKeyPress();
+  }
+
+  setUpKeyPress() {
+    document.getElementsByTagName("body")[0]
+      .addEventListener('keypress',this.triggerKeypress);
+  }
+
+  triggerKeypress(event) {
+    var element = document.getElementsByClassName('focus');
+    if(element[0] !== undefined) {
+      console.log("global keypress");
+      element[0].dispatchEvent(new KeyboardEvent('keypress',{'key':event.key}));
+    }
+    else {
+      console.log("null element");
+    }
+  }
+*/
   render() {
     return (
           <Board />
@@ -42,9 +64,16 @@ class Square extends React.Component {
   }
 
   render() {
+    var classTag = this.class_name(this.props.tag);
+    if(this.props.value.length === 1) {
+      classTag += " locked";
+    }
+    if(this.props.tag === this.props.focus) {
+      classTag += " focus";
+    }
     return (
       <textarea
-        className={this.class_name(this.props.tag)}
+        className={classTag}
         id={this.props.id}
         name={this.props.tag}
         onClick={this.props.clickHandler}
@@ -60,7 +89,9 @@ class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: []
+      squares: [],
+      mode: 'Pencil',
+      focus: null
     };
 
 //  initialize squares
@@ -83,8 +114,7 @@ class Board extends React.Component {
     const ASCII_1 = 49;
     const ASCII_9 = 57;
 
-    console.log("handling keypress");
-    console.log(event.key);
+    console.log("local keypress");
 
     var ascii_value = event.key.charCodeAt(0);
     if(ascii_value < ASCII_1 || ascii_value > ASCII_9) {
@@ -97,30 +127,35 @@ class Board extends React.Component {
     var idx = element.id;
 
     var squares = Array.from(this.state.squares);
-    var value = squares[idx].value;
-    var arr_values = value.split("");
-    var position = arr_values.indexOf(event.key);
-    if(position === -1) {
-      arr_values.push(event.key);
-      arr_values.sort();
+    if(this.state.mode === 'Pencil') {
+      var value = squares[idx].value;
+      var arr_values = value.split("");
+      var position = arr_values.indexOf(event.key);
+      if(position === -1) {
+        arr_values.push(event.key);
+        arr_values.sort();
+      }
+      else {
+        arr_values.splice(position, 1, '');
+      }
+
+      squares[idx].value = arr_values.join("");
     }
     else {
-      arr_values.splice(position, 1, '');
+      squares[idx].value = event.key;
     }
-
-    squares[idx].value = arr_values.join("");
     this.setState({squares: squares});
   }
 
-  handleClick(name) {
+  handleClick(idx) {
     //  toggle the class name 'focus'
-    console.log("handling click");
     var elements = document.getElementsByClassName('focus');
     while ( elements.length > 0 ) {
       elements[0].classList.remove('focus');
     }
-    var element = document.getElementById(name)
+    var element = document.getElementById(idx)
     element.classList.add('focus');
+    this.setState({focus: idx})
   }
 
   renderSquare(idx) {
@@ -131,6 +166,7 @@ class Board extends React.Component {
       id={idx}
       clickHandler={() => this.handleClick(idx)}
       keyPressHandler={(event) => this.handleKeyPress(event)}
+      focus={this.state.focus}
     />;
   }
 
@@ -157,14 +193,41 @@ class Board extends React.Component {
     </div>);
   }
 
+  handleModeButton() {
+    if(this.state.mode === 'Pencil') {
+      this.setState({mode: 'Ink'});
+    }
+    else {
+      this.setState({mode: 'Pencil'});
+    }
+  }
+
   render() {
     return (
-      <div className="game" >
-        {this.createGrid()}
+      <div>
+        <Mode
+          mode={this.state.mode}
+          modeButtonHandler={() => this.handleModeButton()}
+        />
+        <div className="game" >
+          {this.createGrid()}
+        </div>
       </div>
     );
   }
 }
 
+class Mode extends React.Component {
+
+  render () {
+    return(
+      <button
+        className='mode'
+        onClick={this.props.modeButtonHandler}>
+          {this.props.mode}
+      </button>
+    );
+  }
+}
 
 export default App;
